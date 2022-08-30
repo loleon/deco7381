c17f739bfdb0ef512de457f34e4b1ac410e14eba
ruoyiluo <429024233@qq.com>
2022-08-30 11:15:08 +0800

    first

---
+++ src/components/PersonalShow.vue
@@ -0,0 +1,148 @@
+<template>
+	<div style="width:55%;margin:0 auto;height:99.9vh">
+		<h5>Team Member Information</h5>
+		<h1 style="height: 80px;margin:3% 0 0 3%;">Tim</h1>
+		<div style="height: 100%;">
+			<!-- head -->
+			<div style="border-bottom: 2px solid lightgray;display: flex;justify-content: space-between;">
+				<span><span style="line-height: 30px;height:30px;vertical-align: bottom;">Description</span>
+				</span>
+			</div>
+			<!-- part1 -->
+			<div class="bor" style="display: flex;justify-content: space-between;margin-top: 1%;min-height:250px;max-height:450px;">
+				<div style="width:65%;">
+					<ul class="ful">
+						<li>
+							hello im tom!
+						</li>
+					</ul>
+				</div>
+				<div style="width:30%;display: flex;align-items: center;justify-content: center;">
+					<img class="yuanxing" src="../assets/bob.png"  alt="null"/>
+				</div>
+			</div>
+			<!-- part2 -->
+			<div style="border-bottom: 2px solid lightgray;display: flex;justify-content: space-between;margin-top:2%;">
+				<span><span style="line-height: 30px;height:30px;vertical-align: bottom;">Personal Goals</span>
+				</span>
+			</div>
+			<div style="min-height:30%;max-height:45%;margin-top: 1%;border-bottom: 2px solid lightgray;" class="bor">
+				<ul class="ful2">
+					<li v-for="(item,index) in goals" :key="index">Goal {{index+1}}:    {{item}}</li>
+				</ul>
+			</div>
+			<!-- footer -->
+			<h1 style="margin:3% 0 0 3%;">Personal Task Status</h1>
+			<div style="height: 40%;">
+				
+				<ul class="ful3">
+					<li><h3>In progress</h3></li>
+					<li>Task1:prepare midterm exam</li>
+					<li>Task2: complete midterm exam</li>
+					<li>Task3</li>
+				</ul>
+				<ul class="ful3">
+					<li><h3>Future</h3></li>
+					<li>Task1:complete final exam</li>
+					<li>Task2</li>
+					<li>Task3</li>
+				</ul>
+				<ul class="ful3">
+					<li><h3>Done</h3></li>
+					<li>Task1</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'PersonalShow',
+		props: {
+
+		},
+		data() {
+			return {
+				goals: ['go shop mart with mum!'],
+			}
+		},
+		methods: {
+			
+		},
+
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	tr {
+		height: 5%;
+	}
+
+	td {
+		width: 50%;
+	}
+
+	tr:nth-of-type(2),
+	tr:nth-of-type(4) {
+		background-color: lightgray;
+	}
+	.ful3{
+		margin-top:3%;
+	}
+.ful3>li{
+	margin-left:5%;
+	height:30px;
+	list-style: none;
+}
+	button {
+		width: 60px;
+		height: 30px;
+	}
+
+	.ful {
+		width: 100%;
+		margin: 8% 0 0 2%;
+	}
+
+	.ful2 {
+		text-align: center;
+		width: 100%;
+		margin: 2% 0 0 2%;
+	}
+
+	.ful>li {
+		list-style: none;
+		min-height:30px;max-height: 70px;
+		margin: 2% 0;
+		width: 90%;
+		display: flex;
+		justify-content: space-between;
+		
+		overflow-y:scroll;
+	}
+
+	.ful2>li {
+		list-style: none;
+		min-height:30px;max-height: 70px;
+		margin: 2% 0;
+		display: flex;
+		overflow-y:scroll;
+		justify-content: space-between;
+	}
+
+	.imgicon {
+		width: 20px;
+		height: 20px;
+	}
+
+	.yuanxing {
+		width: 200px;
+		height: 200px;
+		border: 1px solid black;
+		margin: 5px 5px 5px 0;
+		position: absolute;
+		border-radius: 110px;
+	}
+</style>
---
+++ src/components/RegisterPage.vue
@@ -0,0 +1,139 @@
+<template>
+	<div
+      style="width:99.5%;height:100vh;background-size: cover;background-position: center 0;" :style="{backgroundImage:'url('+src+')'}">
+		<div style="width:32%;height:100%;margin:0 auto;">
+			<div
+          style="height:75%;position: relative;top:5%;background-color: white;box-shadow: 4px 4px 15px grey;border-radius: 10px">
+				<div style="text-align: center;width:70%;margin:0 auto;padding-top: 2%;">
+					<h1 ><img src="../assets/Team-reg.png"
+                    style="height:30px;width:30px;margin-top: 15px" alt="null"/>
+					<span
+              style="height:60px;vertical-align: top;line-height: 60px;margin-left:7%;font-size: 15px">Sign up</span></h1>
+					<div>
+						<div style="margin-bottom:2%;">
+							<div class="propDiv">Name</div>
+							<input type="text" class="minput icons"
+                     :style="{backgroundImage:'url('+icon1+')'}" placeholder="Enter your full name" />
+						</div>
+						<div style="margin-bottom:2%;">
+							<div class="propDiv">Email</div>
+							<input  type="text" class="minput icons" :style="{backgroundImage:'url('+icon2+')'}" placeholder="Enter your UQ Email" />
+							
+						</div>
+						<div style="margin-bottom:2%;">
+							<div class="propDiv">Code</div>
+							<input  type="text" id="icon3" class="minput icons"
+                      style="width:50%;background-size: 10%;"
+                      :style="{backgroundImage:'url('+icon3+')'}"
+                      placeholder="press 6 digits" />
+							<button
+                  style="background-color: rgb(176,38,227);color:white;width:30%;height: 30px;font-size: 10px;margin-left: 4px;border-radius: 5px;margin-top: 1px">Send Code</button>
+						</div>
+						<div style="margin-bottom:2%;">
+							<div class="propDiv">Password</div>
+							<input  type="password" visiable class="minput icons" :style="{backgroundImage:'url('+icon4+')'}" placeholder="Enter a 6-18 digits password" />
+						</div>
+						<div style="margin-bottom:2%;">
+							<div class="propDiv">RePassword</div>
+							<input  type="password" class="minput icons" :style="{backgroundImage:'url('+icon4+')'}" placeholder="Enter password again" />
+						</div>
+						<div
+                style="font-weight: 400;font-size: 10px;margin-top:3%;">
+							<input type="checkbox" class="layers-item-selector"
+                     /><span
+                style="display: inline-block;margin-bottom: 5px">Tick
+              and
+              agree</span><a
+                href="#"><span
+                style="color:blue;margin-left:3%;">《User Privacy
+              Policy》</span></a>
+						</div>
+						<div style="margin-top:4%;">
+							<button
+                  style="width:60%;height:40px;background-color:rgb(176,38,227);color:white;font-size:13px;border:0;border-radius: 5px">Sign Up</button>
+						</div>
+						<div style="margin-top:2%;font-weight: 500;font-size: 10px">
+							Alreay have accound? <a href="#" style="color:rgb(176,38,227)">Login</a>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'RegisterPage',
+	props: {
+
+	},
+	data() {
+		return {
+			src: require('../assets/bck-reg.jpg'),
+			icon1: require('../assets/nickname.svg'),
+			icon2: require('../assets/phone.svg'),
+			icon3: require('../assets/email-fill.svg'),
+			icon4: require('../assets/LOCK.svg'),
+			icon5: require('../assets/LOCK.svg'),
+		}
+	},
+}
+</script>
+
+<style scoped>
+	.icons{
+		background-size: 6%;
+		background-repeat: no-repeat;
+		background-position: 5px 8px;
+    opacity: 60%;
+
+	}
+  #icon3{
+    background-position: 5px 6px;
+  }
+
+	.propDiv{
+		text-align: left;
+    width: 100%;
+    margin-left: 20px;
+    font-weight: 500;
+    transform-origin: 0 0;
+    transform:scale(0.7,0.7);
+    font-size: 0.1rem;
+	}
+	.minput {
+		border: 1px solid lightgray;
+		width: 80%;
+		height: 30px;
+		line-height: 40px;
+		font-size: 12px;
+		border-radius: 5px;
+		padding-left:8%;
+	}
+  a{
+    text-decoration: none;
+  }
+  .layers-item-selector {
+    outline: none;
+    width: 10px;
+    height: 10px;
+    margin-top:5px;
+    margin-left: 15px;
+    margin-right: 5px;
+    border: solid 1px black;
+    border-radius: 50%;
+    font-size: 0.8rem;
+    padding: 0;
+    cursor:pointer;
+    appearance: none;
+  }
+  .layers-item-selector:checked {
+    /* background: #0242c6; */
+    background: url("../assets/right.svg") no-repeat;
+    background-size: 100% 100%;
+    border: solid 0 black;
+  }
+
+</style>
